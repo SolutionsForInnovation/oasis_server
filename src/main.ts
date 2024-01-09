af32cccd0133bb49086c4138298cff4a47bc39d1
Alfonso Rios <malforime@gmail.com>
2024-01-09 11:56:43 -0600

    📝 Add config for Swagger auto-generated docs for API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Oasis API server')
+    .setDescription('Endpoints for internal consume')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
